57fd7c31b41a656fdbd14d25e3e57a643cb4efed
prashanth <vengalaprashanth24@gmail.com>
2021-08-08 03:15:20 +0530

    resolved conflicts

--- frontend/src/Components/SignUp/Register.jsx
+++ frontend/src/Components/SignUp/Register.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import logImage from "./../imgs/Mobile login-rafiki.png";
-import "./Register.css";
+import logImage from "./../imgs/Secure login-rafiki.png";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
+import style from "./Register.module.css";
 import { signupSuc, signupFail } from "../../Redux/SignUp/action";
 import firebase from "firebase";
 
@@ -30,7 +30,7 @@ const Register = () => {
   const [query, setQuery] = useState(obj);
   const [done, setDone] = useState(false);
 
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
   // useEffect(() => {
   //   auth.onAuthStateChanged((person) => {
@@ -104,89 +104,83 @@ const Register = () => {
   };
 
   return !done ? (
-    <>
-      <div className="container">
-        <div className="left-container">
-          <img src={logImage} alt="img" />
+    <div className={style.container}>
+      <div className={style.left_container}>
+        <img src={logImage} alt="img" />
+      </div>
+      <div className={style.right_container}>
+        <div className={style.heading}>
+          <p>Exam Registration Form</p>
+          <br />
+          Already registered?
+          <Link
+            to="/new-login"
+            style={{ textDecoration: "none", color: "blue", padding: 5 }}
+          >
+            <span>LOGIN</span>
+          </Link>
+          here
         </div>
-        <div className="right-container">
-          <div className="heading">
-            <p>Exam Registration Form</p>
-            <br />
-            Already registered?
-            <Link
-              to="/new-login"
-              style={{ textDecoration: "none", color: "blue", padding: 5 }}
-            >
-              <span>LOGIN</span>
-            </Link>
-            here
-          </div>
-          <div>
-            <form class="ui form">
-              <div class="field">
-                <label>First Name</label>
-                <input
-                  type="text"
-                  placeholder="First Name"
-                  name="first_name"
-                  onChange={handleChange}
-                  value={first_name}
-                />
-              </div>
-              <div class="field">
-                <label>Last Name</label>
-                <input
-                  type="text"
-                  name="last_name"
-                  onChange={handleChange}
-                  value={last_name}
-                  placeholder="Last Name"
-                />
-              </div>
-              <div class="field">
-                <label>Email</label>
-                <input
-                  type="email"
-                  placeholder="eg. admin@gmail.com"
-                  name="email"
-                  onChange={handleChange}
-                  value={email}
-                />
-              </div>
-              <div class="field">
-                <label>Password</label>
-                <input
-                  type="password"
-                  placeholder="eg. admin123"
-                  name="password"
-                  onChange={handleChange}
-                  value={password}
-                />
-              </div>
-
-              <button class="ui yellow button" onClick={handleRegister}>
-                SignUp
-              </button>
-            </form>
-            {/* <div className="or-line">
-              <p>
-                <span>Or</span>
-              </p>
-            </div> */}
-            <div className="google-btn1">
-              <button
-                class="ui google plus button"
-                // onClick={handleGoogleSignUp}
-              >
-                <i class="google plus icon"></i>
-                Google
-              </button>
+        <div>
+          <form class="ui form" className={style.form}>
+            <div class="field">
+              <label>First Name</label>
+              <input
+                type="text"
+                placeholder="First Name"
+                name="first_name"
+                onChange={handleChange}
+                value={first_name}
+              />
             </div>
+            <div class="field">
+              <label>Last Name</label>
+              <input
+                type="text"
+                name="last_name"
+                onChange={handleChange}
+                value={last_name}
+                placeholder="Last Name"
+              />
+            </div>
+            <div class="field">
+              <label>Email</label>
+              <input
+                type="email"
+                placeholder="eg. admin@gmail.com"
+                name="email"
+                onChange={handleChange}
+                value={email}
+              />
+            </div>
+            <div class="field">
+              <label>Password</label>
+              <input
+                type="password"
+                placeholder="eg. admin123"
+                name="password"
+                onChange={handleChange}
+                value={password}
+              />
+            </div>
+
+            <button class="ui yellow button" onClick={handleRegister}>
+              SignUp
+            </button>
+          </form>
+
+          <div className={style.google_btn1}>
+            <button
+              class="ui google plus button"
+              // onClick={handleGoogleSignUp}
+            >
+              <i class="google plus icon"></i>
+              Google
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   ) : (
     <Redirect to="/new-login" />
   );
